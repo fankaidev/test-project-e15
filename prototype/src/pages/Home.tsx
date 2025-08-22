aa83d750af4283ea5bf7c0de677bedfb95486de8
fankai <fankaidev@gmail.com>
2025-08-22 11:54:39 +0800

    fixup

--- prototype/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div>
-      {/* 首页内容将基于提供的 HTML 文件自动生成 */}
-    </div>
-  );
-}
-
-export default Home;
--- prototype/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-// Common types for the application
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-}
-
-// Add your project-specific types here
